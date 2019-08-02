a3254232ef822b7897366cab68f242549085e5d3
moltra <webmaster@tosblog.us>
2019-08-01 23:06:14 -0400

    uploaded the boilerplate for a wordpress plugin

---
+++ AmazonAdvertising/cpaa/includes/class-cpaa-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/moltra
+ * @since      1.0.0
+ *
+ * @package    Cpaa
+ * @subpackage Cpaa/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cpaa
+ * @subpackage Cpaa/includes
+ * @author     Stephen Strickland <admin@podhints.com>
+ */
+class Cpaa_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
